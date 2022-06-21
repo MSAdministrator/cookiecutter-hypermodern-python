5c64c4082e875804d5c62ec7b3b5496e9487973b
MSAdministrator <rickardja@live.com>
2022-06-21 11:02:34 -0500

    dev: Updating spacing and end-of-line

--- {{cookiecutter.project_name}}/tests/test_main.py
+++ {{cookiecutter.project_name}}/tests/test_main.py
@@ -1,5 +1,6 @@
 """Test cases for the __main__ module."""
 from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 from {{cookiecutter.package_name}} import __main__
@@ -7,6 +8,7 @@ from {{cookiecutter.package_name}} import __main__
 
 if TYPE_CHECKING:
     from click.testing import CliRunner
+
 
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
